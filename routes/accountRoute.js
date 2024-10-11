d27e755813801a9aa78e5688244efd032c03f0bb
liezl76 <lzel_76@yahoo.com>
2024-10-11 19:28:33 +0800

    edit account-model, accountRoute and add account-validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
 
 /* ***********************
  * Deliver Login View
@@ -14,8 +15,13 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 /* ***********************
- * Deliver Registration Routes
+ * Process the registration data
  * ************************/
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 module.exports = router
